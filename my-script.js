fb0190afefe4ce97f9427bfd1e0d5755a80425ad
Kimberley <84804555+kimberleyl@users.noreply.github.com>
2023-01-07 13:07:20 +0000

    Add files via upload

---
+++ my-script.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Date} dateObject
+ */
+
+function formatDate(dateObject) {
+    const parts = {
+        date: dateObject.getDate(),
+        month: dateObject.getMonth() +1,
+        year: dateObject.getFullYear(),
+        hour: (dateObject.getHours()%12)||12,
+        minute: dateObject.getMinutes().toString().padStart(2,"0"),
+        amOrPm: dateObject.getHours()<12?"AM":"PM"
+    };
+    return '${parts.date}/${parts.month}/${parts.year} ${parts.hour}:${parts.minute} ${parts.amOrPm}';
+}
+
+const myDate = new Date();
+const myDateFormatted = formatDate(myDate);
+
+console.log(myDateFormatted);
